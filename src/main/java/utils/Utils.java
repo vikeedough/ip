6a28206af54553c18b63b22ae16b83477bde6f28
vikeedough <156529589+vikeedough@users.noreply.github.com>
2025-01-19 18:59:43 +0800

    Create utils class

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,16 @@
+public class Utils {
+
+    private void printLine() {
+        System.out.println("--------------------------------------------------------");
+    }
+    public void printGreeting() {
+        printLine();
+        System.out.println("Hello! I'm Dudu!");
+        System.out.println("How may I assist you today?");
+    }
+
+    public void printExit() {
+        printLine();
+        System.out.println("See you next time!");
+    }
+}
